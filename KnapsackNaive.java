286c18c565e33b19d99aeda7ec85b7ab6953b086
AnakinCodeWriter <alfie.richards1@gmail.com>
2025-04-30 16:35:02 +0100

    Update KnapsackNaive.java
    Adding comments

--- KnapsackNaive.java
+++ KnapsackNaive.java
@@ -1,8 +1,8 @@
 public class KnapsackNaive{
-    public static int largest(int a, int b) { return (a > b) ? a : b; }
+    public static int largest(int a, int b) { return (a > b) ? a : b; } //this function will return whichever variable is larger (a or b)
 
     static int knapSack(int maxWeight, int weight[], int value[], int n){
-        if (n == 0 || maxWeight == 0) return 0;
+        if (n == 0 || maxWeight == 0) return 0; //this will return the value 0 if the problem is at it's base case (maximum weight of 0 or no items to choose from)
 
         if (weight[n-1] > maxWeight) return knapSack(maxWeight, weight, value, n-1);
 
@@ -12,7 +12,7 @@ public class KnapsackNaive{
     }
 
     public static void main(String args[]){
-        int value[] = new int[] {4, 7, 5};
+        int value[] = new int[] {4, 7, 5}; //this is the values of the items entering into the knapsack function
         int weight[] = new int[] {14, 34, 17};
         int maxWeight = 140;
         int n = value.length;
